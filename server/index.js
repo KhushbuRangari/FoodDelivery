25fbc83090109916763520a4f3974c4345aa7337
Khushbu Rangari <khushbu.rangari@gmail.com>
2023-12-26 08:05:50 +0530

    APIs Done

--- server/index.js
+++ server/index.js
@@ -12,12 +12,23 @@ dotENV.config();
 const dbConnection = require('./dbConnection');
 dbConnection();
 
+
+
+
 const PORT= process.env.PORT;
+const userRouter=require("./router/user.routes");
+app.use('/user',userRouter);
 
 
+const ownerRouter=require("./router/owner.routes");
+app.use('/owner',ownerRouter);
 
 
-app.listen(()=>{
+const orderRouter=require("./router/order.routes");
+app.use('/order',orderRouter);
+
+
+app.listen(PORT,()=>{
     try {
         console.log(`Server is running on ${PORT}`);
     } catch (error) {
